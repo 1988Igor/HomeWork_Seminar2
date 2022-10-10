a27404484a14055429a2a2bc547d9b6b0be18a1a
Igor <romanigor.conect@gmail.com>
2022-10-10 20:43:12 +0200

    added Task2

---
+++ Task2.py
@@ -0,0 +1,13 @@
+# Напишите программу, которая принимает на вход число N и 
+# выдает набор произведений чисел от 1 до N.
+
+# Пример:
+# - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+count = 0
+multiply = 1
+number = 4
+while count < number:
+    for i in range(1, number + 1):
+        multiply *= i
+        print(f'{multiply}', end=",")
+        count += 1
