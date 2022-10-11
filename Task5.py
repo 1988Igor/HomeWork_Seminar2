1bf7ff54cb6d798a352182e0362e8e62c94feefd
Igor <romanigor.conect@gmail.com>
2022-10-11 22:29:09 +0200

    added some corrections

--- Task5.py
+++ Task5.py
@@ -1,8 +1,9 @@
 # Реализуйте алгоритм перемешивания списка.
-from random import shuffle
+
+import random
 lst = [1, 2, 3, 4, 5, 6, 7, 8]
 print(f'The first list {lst}')
 lst2 = []
 lst2 = lst
-shuffle(lst2)
-print(f'The changed list {lst2}')
+random.shuffle(lst2)
+print(f'The shuffled list {lst2}')
