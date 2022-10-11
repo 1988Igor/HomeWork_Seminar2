99f6a6eb7fd14b826a2162f26a1c0e21ef74bc20
Igor <romanigor.conect@gmail.com>
2022-10-11 22:25:49 +0200

    added corrections

--- Task3.py
+++ Task3.py
@@ -1,13 +1,16 @@
 # Задайте список из n чисел последовательности
 # $(1+\frac 1 n)^n$ и выведите на экран их сумму.
+
 count = 0
 multiply = 0
-number = 6
-lst = []
+number = int(input('Enter a number: '))
+dictionary_ = {}
 while count < number:
     for i in range(1, number+1):
-        multiply = (1+(1/i))**i
-        lst.append(round(multiply, 2))
+        dictionary_[i] = (1+(1/i))**i
         count += 1
-print(lst)
-print(f'The sum of numbers in the list is : ', sum(lst))
+round_ = 2
+for elem in dictionary_:
+    dictionary_[elem] = round(dictionary_[elem], 2)
+print(dictionary_)
+print(f'The sum of numbers in the dictionary:', sum(dictionary_.values()))
