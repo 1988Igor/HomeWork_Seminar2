028f16a52725533b736ae4338c3462294c557cd0
Igor <romanigor.conect@gmail.com>
2022-10-11 22:07:04 +0200

    added some corrections

--- Task2.py
+++ Task2.py
@@ -3,11 +3,14 @@
 
 # Пример:
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
 count = 0
 multiply = 1
 number = 4
+lst = []
 while count < number:
     for i in range(1, number + 1):
         multiply *= i
-        print(f'{multiply}', end=",")
+        lst.append(multiply)
         count += 1
+    print(f'The sum of product of numbers from 1 to N is :\n {lst}')
